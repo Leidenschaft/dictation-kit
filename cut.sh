9110b954f092cf6841eb3581b853b5d4b1662ddd
zhaofeng-shu33 <616545598@qq.com>
2021-05-13 20:46:06 +0800

    update cut code

--- cut.sh
+++ cut.sh
@@ -1,6 +1,8 @@
 set -e -x
-filename="j2-16-1.MP3"
-file="../../Japanese/j2-audio-wav/$filename.wav"
-echo "$file" > test.dbl
-../julius/julius/julius -C main.jconf -C am-dnn.jconf -dnnconf julius.dnnconf > a.txt
-python3 get_split_time.py | xargs -I{} ffmpeg -y -i "../../Japanese/j2-audio-wav/$filename" -t {} "../../Japanese/j2-audio-cut/$filename"
+for filename_total in ../../Japanese/j2-audio/*; do
+    filename=$(basename "$filename_total")
+    file="../../Japanese/j2-audio-wav/$filename.wav"
+    echo "$file" > test.dbl
+    ../julius/julius/julius -C main.jconf -C am-dnn.jconf -dnnconf julius.dnnconf > "$filename.txt"
+    python3 get_split_time.py "$filename.txt" | xargs -I{} ffmpeg -y -i "../../Japanese/j2-audio-wav/$filename" -t {} "../../Japanese/j2-audio-cut/$filename"
+done
